42674f7d5addbff42d9442cb81ca074a2f3fe22e
Michal Becmer <michalcloud2003@gmail.com>
2026-02-06 23:46:19 +0000

    ✨ Added S.A.M to project and created a basic method for using the voice

---
+++ Assets/ProjectFiles/Scripts/Voice/Voice.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using SamSharp;
+
+public class Voice : MonoBehaviour
+{
+    public AudioSource source;
+    Sam sam = new Sam();
+
+    void Start()
+    {
+        //Debug
+        Speak("Hello I am Sam");
+    }
+
+    public void Speak(string text)
+    {
+        byte[] bytes = sam.Speak(text);
+
+        float[] samples = ConvertToFloats(bytes);
+
+        AudioClip clip = AudioClip.Create(
+            "SAM",
+            samples.Length,
+            1,
+            22050,
+            false
+        );
+
+        clip.SetData(samples, 0);
+        source.PlayOneShot(clip);
+    }
+
+    float[] ConvertToFloats(byte[] data)
+    {
+        float[] f = new float[data.Length];
+        for (int i = 0; i < data.Length; i++)
+            f[i] = (data[i] - 128) / 128f;
+
+        return f;
+    }
+}
