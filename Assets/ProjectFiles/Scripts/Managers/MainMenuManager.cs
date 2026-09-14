0604bc603868cb6c00d888b28a68042e6f043e38
Clouddeboi <michalcloud2003@gmail.com>
2026-09-14 14:56:14 +0100

    ✨ Added a basic main menu

---
+++ Assets/ProjectFiles/Scripts/Managers/MainMenuManager.cs
@@ -0,0 +1,70 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [Header("Root Panel")]
+    [SerializeField] private GameObject mainPanel;
+
+    [Header("Scene Loading")]
+    [SerializeField] private string gameSceneName = "Game";
+
+    private readonly Stack<GameObject> panelStack = new Stack<GameObject>();
+
+    private void Awake()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject != mainPanel)
+                child.gameObject.SetActive(false);
+        }
+
+        mainPanel.SetActive(true);
+        panelStack.Push(mainPanel);
+    }
+
+    private void Update()
+    {
+        if (InputManager.Instance == null)
+            return;
+
+        if (InputManager.Instance.CancelAction.WasPressedThisFrame())
+        {
+            GoBack();
+        }
+    }
+
+    public void OpenPanel(GameObject panel)
+    {
+        if (panel == null || panelStack.Peek() == panel)
+            return;
+
+        panelStack.Peek().SetActive(false);
+        panel.SetActive(true);
+        panelStack.Push(panel);
+    }
+
+    public void GoBack()
+    {
+        if (panelStack.Count <= 1)
+            return;
+
+        panelStack.Pop().SetActive(false);
+        panelStack.Peek().SetActive(true);
+    }
+
+    public void OnPlayClicked()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void OnExitClicked()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
